f46bcd12d8f8a01d6dfe221c7e54e757d92c69df
DanyCt <dcaizatoa.torres@gmail.com>
2024-05-22 12:53:27 -0500

    4th commit

--- src/main/java/com/example/myfirstproject1_0/HelloApplication.java
+++ src/main/java/com/example/myfirstproject1_0/HelloApplication.java
@@ -10,30 +10,23 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
     }
-    public class SumaValores {
-        public static void main(String[] args) {
-            // Crear un objeto Scanner para leer la entrada del usuario
-            Scanner input = new Scanner(System.in);
-
-            // Solicitar el primer número al usuario
-            System.out.print("Ingresa el primer número: ");
-            int numero1 = input.nextInt();
-
-            // Solicitar el segundo número al usuario
-            System.out.print("Ingresa el segundo número: ");
-            int numero2 = input.nextInt();
-
-            // Calcular la suma de los dos números
-            int suma = numero1 + numero2;
-
-            // Imprimir el resultado
-            System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
-
-            // Cerrar el objeto Scanner
-            scanner.close();
-        }
-    }
     public static void main(String[] args) {
-        launch();
+        // Crear un objeto Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+
+              System.out.print("Enter the first value: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Enter the second value: ");
+        double numero2 = scanner.nextDouble();
+
+        // Sumar los dos números
+        double suma = numero1 + numero2;
+        double divit = numero1 / numero2;
+        // Mostrar el resultado de la suma y division
+        System.out.println("The sum is => " + numero1 + " y " + numero2 + " es: " + suma);
+        System.out.println("The divtion is => " + numero1 + " and " + numero2 + " is: " + divit);
+        // Cerrar el scanner
+        scanner.close();
     }
 }
